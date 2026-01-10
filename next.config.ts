ad46238540c7c85a1a52f78054c47b1f8d0f8841
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-01-10 23:06:00 +0300

    fix configuration issues to allow image viewing in the featured products section

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
